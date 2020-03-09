56cb9f6cf7d3e9de44ce94cc5e25426e62db2649
Hongyan Li <hongyan99@yahoo.com>
2020-03-08 22:55:57 -0700

    updated test data

--- src/main/java/review/graph/advance/Dijkstra.java
+++ src/main/java/review/graph/advance/Dijkstra.java
@@ -1,6 +1,7 @@
 package review.graph.advance;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -12,31 +13,26 @@ import review.graph.WeightedEdge;
 public class Dijkstra {
 	public static void main(String[] args) {
 		List<WeightedEdge> edges = new ArrayList<>();
-		edges.add(new WeightedEdge(1, 7, 6));
+		edges.add(new WeightedEdge(4, 0, 1));
+		edges.add(new WeightedEdge(20, 7, 6));
 		edges.add(new WeightedEdge(2, 8, 2));
 		edges.add(new WeightedEdge(2, 6, 5));
-		edges.add(new WeightedEdge(4, 0, 1));
 		edges.add(new WeightedEdge(4, 2, 5));
 		edges.add(new WeightedEdge(6, 8, 6));
 		edges.add(new WeightedEdge(7, 2, 3));
 		edges.add(new WeightedEdge(7, 7, 8));
 		edges.add(new WeightedEdge(8, 0, 7));
-		edges.add(new WeightedEdge(8, 1, 2));
+		edges.add(new WeightedEdge(1, 1, 2));
 		edges.add(new WeightedEdge(9, 3, 4));
 		edges.add(new WeightedEdge(10, 5, 4));
-		edges.add(new WeightedEdge(11, 1, 7));
+		edges.add(new WeightedEdge(1, 1, 7));
 		edges.add(new WeightedEdge(14, 3, 5));
 		
-		List<WeightedEdge> result = mst(edges, 9);
-		result.forEach(e-> {
-			System.out.print(e.getDistance()+", ");
-			System.out.print(e.getValue()+", ");
-			System.out.print(e.getSource()+", ");
-			System.out.println(e.getDest());
-		});
+		int[] distance = shortest(edges, 9);
+		System.out.println(Arrays.toString(distance));
 	}
 	
-	public static List<WeightedEdge> mst(List<WeightedEdge> edges, int dim) {
+	public static int[] shortest(List<WeightedEdge> edges, int dim) {
 		if(dim<1 || edges.size()<dim-1) {
 			return null;
 		}
@@ -59,10 +55,11 @@ public class Dijkstra {
 		});
 		
 		// 1.c) Initialize the captured and the queue
+		int[] distances = new int[dim];
 		boolean[] captured = new boolean[dim];
 		WeightedEdge root = edges.get(0);
 		captured[root.getSource()] = true;
-		addEdgesFor(0, root.getSource(), adjListMap, captured, queue);
+		addEdgesFor(0, root.getSource(), adjListMap, captured, queue, distances);
 
 		// 2) Loop through the edges in the queue
 		List<WeightedEdge> result = new ArrayList<>();
@@ -70,10 +67,10 @@ public class Dijkstra {
 			WeightedEdge edge = getNext(queue, captured);
 			if(edge!=null) {
 				result.add(edge);
-				addEdgesFor(edge.getDistance(), edge.getDest(), adjListMap, captured, queue);
+				addEdgesFor(edge.getDistance(), edge.getDest(), adjListMap, captured, queue, distances);
 			}
 		}
-		return result;
+		return distances;
 	}
 	
 	// Fetch the next shortest path that at least one end is not captured 
@@ -92,7 +89,7 @@ public class Dijkstra {
 	
 	// Add all edges from the newly captured node to the priority queue.
 	private static void addEdgesFor(int distance, int node, Map<Integer, List<WeightedEdge>> adjListMap, 
-			boolean[] captured, PriorityQueue<WeightedEdge> queue) {
+			boolean[] captured, PriorityQueue<WeightedEdge> queue, int[] distances) {
 		List<WeightedEdge> adjList = adjListMap.get(node);
 		if(adjList!=null) {
 			adjList.forEach(e-> {
@@ -100,7 +97,8 @@ public class Dijkstra {
 					// Only is one end is not captured we add the edge to the queue
 					WeightedEdge ee = new WeightedEdge(e.getValue(), e.getSource(), e.getDest());
 					ee.setDistance(distance+e.getValue());
-					queue.add(ee);	
+					distances[ee.getDest()] = ee.getDistance();
+					queue.add(ee);
 				}
 			});
 		}
